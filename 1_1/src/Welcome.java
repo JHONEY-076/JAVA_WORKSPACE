8fbf1ac595c41494dcee90b8b8a2f968ab945a83
JHONEY-076 <ccjh2593@gmail.com>
2024-09-09 17:15:59 +0900

    09-09

--- 1_1/src/Welcome.java
+++ 1_1/src/Welcome.java
@@ -1,10 +1,10 @@
 
-public class practice {
+public class Welcome {
 
    public static void main(String[] args) {
       // TODO Auto-generated method stub
       System.out.println("자바는 처음이죠?");
-      System.out.println("Welcom to Java World~~");
+      System.out.println("Welcome to Java World~~");
 
 
    }
--- SampleProject/src/CircleArea.java
+++ SampleProject/src/CircleArea.java
@@ -9,7 +9,7 @@ public class CircleArea {
    
    public static void main(String[] args) {
       // TODO Auto-generated method stub
-      final double PI=3.14;
+      final double PI=3.14; //원주율을 상수로 선언
       double radius= 10.0;
       double circleArea= radius*radius*PI;
       
