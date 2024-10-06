79bd6fbbf830025dbe229e07bcaaf58991a743c8
JHONEY-076 <ccjh2593gmail.com>
2024-10-06 11:33:20 +0900

    10-06

---
+++ JAVA_WORKSPACE/SampleProject/src/ArrayAccess.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ArrayAccess {
+
+	public static void main (String [] args) {
+		
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		int arr[]=new int[5];
+		
+		int max=0;
+		
+		System.out.println("양수 5개를 입력하세요.");
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			 arr[i]=scanner.nextInt();
+			 if(arr[i]>max) {
+				 max=arr[i];
+			 }
+		}
+		System.out.print("가장 큰수는 "+max+" 입니다");
+		
+		scanner.close();
+		
+	}
+	
+}
