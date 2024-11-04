831cd30cd123daaafe7836ff338ce724f43dcb93
JHONEY-076 <ccjh2593@gmail.com>
2024-11-04 09:52:01 +0900

    Save local changes before merge

--- JAVA_WORKSPACE/SampleProject/src/ex_2_5.java
+++ JAVA_WORKSPACE/SampleProject/src/ex_2_5.java
@@ -1,45 +1,31 @@
 import java.util.Scanner;
-public class ex_2_5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Grading {
+    public static void main(String[] args) {
+        char grade;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("점수를 입력하세요(0~100): ");
+        int score = scanner.nextInt(); // 점수 읽기
 
-		Scanner scanner= new Scanner(System.in);
-		
-		System.out.print("학생1>>");
-		String s1_name=scanner.next();
-		
-		int s1_late_score= scanner.nextInt();
-		int s1_absent_score=scanner.nextInt();
-		
-		
-		System.out.print("학생2>>");
-		String s2_name=scanner.next();
-		
-		int s2_late_score= scanner.nextInt();
-		int s2_absent_score=scanner.nextInt();
-		
-		
-		int s1_total_score= 100-(s1_late_score*3)-(s1_absent_score*8);
-		int s2_total_score=100-(s2_late_score*3)-(s2_absent_score*8);
-		
-	
-		
-	
-		if(s1_total_score>s2_total_score) {
-			System.out.print(s1_name+"의 출석 점수가 더 높음.");
-			System.out.print(s1_name+" 출석 점수는 "+s1_total_score);
-		}
-		else if(s1_total_score<s2_total_score){
-			System.out.print(s2_name+"의 출석 점수가 더 높음. ");
-			System.out.print(s2_name+" 출석 점수는 "+s2_total_score);
-		}
-		else if(s1_total_score==s2_total_score) {
-			System.out.print("점수 동일");
-		}
-	
-		scanner.close();
-	
-	}
-	
+        if (score >= 90 && score <= 100) { // score가 90 이상 100 이하
+            grade = 'A';
+        } else if (score >= 80 && score < 90) { // score가 80 이상 90 미만
+            grade = 'B';
+        } else if (score >= 70 && score < 80) { // score가 70 이상 80 미만
+            grade = 'C';
+        } else if (score >= 60 && score < 70) { // score가 60 이상 70 미만
+            grade = 'D';
+        } else { 
+            grade = 'F';
+        // 유효하지 않은 점수 입력 시
+        
+        	System.out.println("잘못된 점수입니다. 0에서 100 사이의 값을 입력하세요.");
+            scanner.close();
+            return; // 프로그램 종료
+        }
+
+        System.out.println("학점은 " + grade + "입니다.");
+        scanner.close();
+    }
 }
